c5bacd011de1cf1711cfce4a87e146f22dfe755f
curryghost <anikin8911@gmail.com>
2022-04-13 03:05:28 +0800

    update navbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,13 +4,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import {MatBadgeModule} from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 const material = [
   MatToolbarModule,
   MatIconModule,
   MatButtonModule,
   MatMenuModule,
-  MatBadgeModule
+  MatBadgeModule,
+  MatSidenavModule
 ]
 
 @NgModule({
