735c3297e5f4ed6e7122d26d565f171b50847946
curryghost <anikin8911@gmail.com>
2022-04-12 15:08:53 +0800

    update navbar

--- src/app/comps/navbar/navbar.component.ts
+++ src/app/comps/navbar/navbar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  links = ["Home", "About", "Courses"]
 
   constructor() { }
 
