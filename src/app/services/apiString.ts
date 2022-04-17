da00036ea2a23fbd798d0f17889974caa4f90479
daniel teo <teodaniel14@gmail.com>
2022-04-17 21:03:13 +0800

    merged changes for 0Auth and api routes

--- src/app/services/apiString.ts
+++ src/app/services/apiString.ts
@@ -1,3 +1,4 @@
-export const port = 5000
-export const domain = 'localhost'
-export const api = `http://${domain}:${port}/api`
+// export const port = 5000
+export const port = 32236;
+export const domain = 'localhost';
+export const api = `http://${domain}:${port}/api`;
--- src/app/services/author.service.ts
+++ src/app/services/author.service.ts
@@ -7,13 +7,8 @@ import { api } from './apiString';
 })
 export class AuthorService {
   // Daniel's api
-<<<<<<< HEAD
-  // baseUrl: string = 'http://localhost:5000/api/Author';
-  baseUrl: string = 'http://localhost:32236/api/Author';
-=======
   baseUrl: string = `${api}/author`;
   // baseUrl: string = 'http://localhost:32236/api/Author';
->>>>>>> d39dd13bd9bb42382a5a9cc2374dd9cb7d8af3f9
 
   constructor(private http: HttpClient) {}
 
